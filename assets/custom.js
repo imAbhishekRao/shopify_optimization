69a1f606c4527da2f95f8c88379018e4350c44a6
Abhishek Gobind Rao <abhishek.rao.4200@gmail.com>
2025-07-08 16:54:27 +0530

    gtm added again

--- assets/custom.js
+++ assets/custom.js
@@ -826,3 +826,14 @@ var updateProductMedia = (variantId) => {
 		}
 	});
 }
+
+document.querySelector('#SizeSelector')?.addEventListener('change', function(e) {
+  var size = e.target.value;
+  if (size !== '') {
+    dataLayer.push({
+      event: 'select_product_size',
+      product_size: size,
+      user_id: '{{ customer.email | sha256 }}' || 'unknown'
+    });
+  }
+});
